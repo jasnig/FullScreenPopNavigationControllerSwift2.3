4145a11e5440e5161e8bace27cdf478461b678ce
ZeroJ <854136959@qq.com>
2016-06-25 17:05:16 +0800

    Initial Commit

---
+++ FullPopSwift2.3Tests/FullPopSwift2_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FullPopSwift2_3Tests.swift
+//  FullPopSwift2.3Tests
+//
+//  Created by jasnig on 16/6/25.
+//  Copyright © 2016年 ZeroJ. All rights reserved.
+//
+
+import XCTest
+@testable import FullPopSwift2_3
+
+class FullPopSwift2_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
